e40d141f61ab9c73e11d1084338a28de842551fa
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-04-16 20:01:59 +0530

    feat: reads game data into memory

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,22 @@
 const std = @import("std");
 
+pub const log_level: std.log.Level = .info;
+
 // https://en.wikipedia.org/wiki/CHIP-8
 // https://github.com/mattmikolay/chip-8/wiki/Mastering-CHIP‐8
-// https://austinmorlan.com/posts/chip8_emulator/
 // Chip-8 was capable of accessing 4KB of RAM (4096 Bytes)
 // Location (0x000) to (0xFFF) (0 to 4095)
-const Chip8 = struct {
+const Memory = struct {
     // 4KB Memory
     memory: [0x1000]u8,
 
     //Registers in CPU
-    V: [16]u8,
+    v: [16]u8,
 
     // CHIP-8 allows memory addressing 2^12,
     // so, 16 bits to cover all the address ranges
-    IR: u16,
-    PC: u16,
+    ir: u16,
+    pc: u16,
 
     stack: [16]u16,
     sp: u8,
@@ -28,6 +29,46 @@ const Chip8 = struct {
     keys: [16]bool,
 };
 
+const LoadRomError = error{
+    OutOfMemory,
+};
+
+const Chip8 = struct {
+    ram: Memory,
+
+    pub fn init() Chip8 {
+        const ram = Memory{
+            .memory = [_]u8{0} ** 0x1000,
+            .v = undefined,
+            .ir = 0,
+            .pc = 0x200,
+            .stack = undefined,
+            .sp = 0,
+            .delay_timer = 0,
+            .sound_timer = 0,
+            .keys = undefined,
+        };
+        return Chip8{ .ram = ram };
+    }
+
+    pub fn load_game(self: *Chip8, gameData: []u8) !void {
+        var offset: usize = 0x200;
+        const endAddress: usize = offset + gameData.len;
+
+        if (endAddress > self.ram.memory.len) {
+            return LoadRomError.OutOfMemory;
+        }
+
+        for (0.., gameData) |index, byte| {
+            self.ram.memory[offset + index] = byte;
+        }
+
+        std.log.info("memory {any}", .{self.ram.memory});
+        std.log.info("gameData {any}", .{gameData});
+        return;
+    }
+};
+
 // Step 1: Read a game file
 
 pub fn readFile(filename: []const u8, allocator: std.mem.Allocator) ![]u8 {
@@ -35,22 +76,25 @@ pub fn readFile(filename: []const u8, allocator: std.mem.Allocator) ![]u8 {
     defer file.close();
 
     const fileSize = (try file.stat()).size;
-    std.debug.print("fileSize {}\n", .{fileSize});
+    std.log.info("fileSize {}\n", .{fileSize});
 
     const buffer = try file.readToEndAlloc(allocator, fileSize);
     return buffer;
 }
 
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
+    std.log.info("Hello, world!\n", .{});
     const filename = "./c8games/TICTAC";
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const allocator = gpa.allocator();
-    const fileContents = try readFile(filename, allocator);
-    defer allocator.free(fileContents);
+    const gameData = try readFile(filename, allocator);
+    defer allocator.free(gameData);
 
-    std.debug.print("File contents: {any}\n", .{fileContents});
+    // std.log.info("File contents: {any}\n", .{fileContents});
+
+    var chip8 = Chip8.init();
+    try chip8.load_game(gameData);
 }
