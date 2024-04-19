9ea4b48daa9c3858828874c4dd7a32122a2ae505
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-04-19 07:16:52 +0530

    broken: reduces the main window size for now

--- src/display.zig
+++ src/display.zig
@@ -57,8 +57,8 @@ pub const Display = struct {
             "chipate",
             .{ .centered = {} },
             .{ .centered = {} },
-            2 * 640,
-            2 * 320,
+            640,
+            320,
             .{ .vis = .shown },
         );
 
